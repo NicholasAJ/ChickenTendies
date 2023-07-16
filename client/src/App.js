61ac537402883f14696781c320c08aca90178af7
Nicholas Juliana <naj1243@gmail.com>
2023-07-16 12:28:05 -0400

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import React, {useState} from 'react';
 
-const [loggedIn, setLoggedIn] = useState(false);
-const [user, setUser] = useState({});
+// const [loggedIn, setLoggedIn] = useState(false);
+// const [user, setUser] = useState({});
+
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-          Chicken Tinder
-        </header>
         <div className='page'>
-          <BrowserRouter>
+      <BrowserRouter>
+        <header className="navbar">
+          Chicken Tinder
+          <p>
+            <NavLink to="/">Home</NavLink>
+          </p>
+        </header>
             <Routes> 
               <Route path="/"/>
               {/* <Route path="/" />
